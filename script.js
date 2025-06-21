aacc11441c86e2267ceb184fc3d2b071ccada8e0
Gautam kumar <165561578+gautamkumar83431@users.noreply.github.com>
2025-06-21 18:57:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const byteSize = (str) => {
   // write your code here
-};
+	const blob = new Blob([str]);
+      return blob.size;
+}
+ function calculateByteSize() {
+      const input = document.getElementById("inputString").value;
+      const size = byteSize(input);
+      document.getElementById("result").textContent = `Byte Size: ${size} bytes`;
+    }
 
 // Do not change the code below
 const str = prompt("Enter some string.");
